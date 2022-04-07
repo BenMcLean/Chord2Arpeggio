913f20e2935a67eef5a58d008367fd3ec5cb9f17
Benjamin McLean <mclean.ben@gmail.com>
2022-04-06 22:06:01 -0500

    Explanation that I'm using this for FamiStudio :)

--- chord2arpeggio.py
+++ chord2arpeggio.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Guitar Chord to Arpeggio
-Inputs a guitar chord name and outputs a series of numbers for how many half steps above the root note each note in the guitar chord is.
+Inputs a guitar chord name and outputs a series of numbers for how many half steps apart the notes are.
+This facilitates creating arpeggios for the chords in FamiStudio. https://famistudio.org/
 Uses chord-fingers.csv from https://archive.ics.uci.edu/ml/datasets/Guitar+Chords+finger+positions
 """
 import argparse
@@ -10,7 +11,7 @@ parser.add_argument('chord', metavar='chord', type=str, nargs='+', help='Name of
 input = parser.parse_args().chord[0]
 standardtuning = [0,5,10,15,19,24] # Every Amateur Does Get Better Eventually
 keytransposes = {'E':0,'F':1,'F#':2,'Gb':2,'G':3,'G#':4,'Ab':4,'A':5,'A#':6,'Bb':6,'B':7,'Cb':7,'C':8,'C#':9,'Db':9,'D':10,'D#':11,'Eb':11}
-octavetranspose=-1
+octavetranspose = -1
 with open('chord-fingers.csv', newline='') as csvfile:
     chordreader = csv.DictReader(csvfile, delimiter=';')
     for chord in chordreader:
