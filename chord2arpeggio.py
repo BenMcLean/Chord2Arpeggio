d7c90a0b10e676879f32d2a160cffb75bb0f2152
Benjamin McLean <mclean.ben@gmail.com>
2022-04-06 19:25:25 -0500

    find chords

--- chord2arpeggio.py
+++ chord2arpeggio.py
@@ -3,7 +3,11 @@
 Inputs a chord name and outputs a series of numbers for how many steps from the base note each note is.
 """
 import argparse
+import csv
 parser = argparse.ArgumentParser(description='Chord')
-parser.add_argument('chord', metavar='N', type=str, nargs='+', help='Name of chord')
-chord = parser.parse_args().chord[0]
-print('Chord is ' + chord)
+parser.add_argument('chord', metavar='chord', type=str, nargs='+', help='Name of chord')
+input = parser.parse_args().chord[0]
+with open('chord-fingers.csv', newline='') as csvfile:
+    chordreader = csv.DictReader(csvfile, delimiter=';')
+    foundchords = next((chord for chord in chordreader if input == chord['CHORD_ROOT'] + chord['CHORD_TYPE']), None)
+print(foundchords)
