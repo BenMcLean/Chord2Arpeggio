52eb9a8075ffcc2cbaf42dba89dad8c83ac82976
Benjamin McLean <mclean.ben@gmail.com>
2022-04-16 11:14:15 -0500

    'deploy'

--- deploy.bat
+++ deploy.bat
@@ -1,10 +1,8 @@
 @ECHO ON
 %~d0:
 cd %~dp0
-call npm run build
-cd dist
-git init
 git checkout -b master
+call npm run build
 git add -A
 git commit -m 'deploy'
 git subtree push --prefix dist origin gh-pages
